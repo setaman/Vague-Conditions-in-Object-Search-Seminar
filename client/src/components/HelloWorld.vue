1647915778b019f345888eab9fa8dfc8f278475a
Sergej Atamantschuk <sergatam@yandex.com>
2018-11-04 17:13:21 +0100

    initialized vuetify app

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-icon>
+          edit
+        </v-icon>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+    })
+  }
+</script>
+
+<style>
+
+</style>
