e6ed4149fd900e2c70f6567567c9123075f56a53
Sergej Atamantschuk <sergatam@yandex.com>
2018-11-04 17:54:38 +0100

    added recombee sdk, initialized express app

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import './api';
 
 Vue.config.productionTip = false
 
