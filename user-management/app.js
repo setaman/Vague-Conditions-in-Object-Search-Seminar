935c868ce56fb25d7085fe90e1235c9575875017
Sergej Atamantschuk <sergej.atamantschuk@isst.fraunhofer.de>
2018-11-24 15:14:55 -0800

    fixed verification

--- user-management/app.js
+++ user-management/app.js
@@ -14,54 +14,19 @@ let JwtStrategy = require('passport-jwt').Strategy,
 
 let db;
 
-// Database Name
 const dbName = 'usersDb';
-
-// Create a new MongoClient
 
 const client = new MongoClient('mongodb://localhost:27017/users', {useNewUrlParser: true});
 
 client.connect(function (err) {
-
     if (err) {
         log.info('mongo', 'Can not connect to db');
     } else {
-        log.info('mongo', 'Connected successful to server');
+        log.info('mongo', 'Connected successful to mongo!');
         db = client.db(dbName);
         //client.close();
     }
 });
-
-/*passport.use('signup', new LocalStrategy({
-        usernameField: 'name',
-        passwordField: 'password',
-    },
-    async (name, password, done) => {
-        log.info('signup', {name});
-        let user;
-        try {
-            user = await db.collection('users').findOne({name});
-            console.log({user});
-            if (user) {
-                console.log(user);
-                return done(null, false, {message: 'User already exist'});
-            }
-        } catch (e) {
-            return done(null, false, {message: e});
-        }
-
-        try {
-            console.log({user});
-            user = await db.collection('users').insertOne({username: name, password})
-            if (!user) {
-                return done(null, false, {message: 'Some Error occurred while inserting new user.'});
-            }
-            return done(null, {message: 'User' + user.name + ' successfully added'});
-        } catch (e) {
-            return done(null, false, {message: e});
-        }
-    }
-));*/
 
 passport.use('signup', new LocalStrategy({
         usernameField: 'name',
@@ -119,21 +84,14 @@ passport.use(new JwtStrategy({
         secretOrKey: 'secret',
         jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
     },
-    (jwt_payload, done) => {
-
-        log.info('JWT strategy', jwt_payload);
-
-        db.findOne({name: jwt_payload.name}, (err, user) => {
-            if (err) {
-                return done(null, false, {message: 'Incorrect username.'});
-            }
-
-            if (!user.password !== passport) {
-                return done(null, false, {message: 'Incorrect password.'});
-            }
-
-            return done(null, user);
-        })
+    async (jwt_payload, done) => {
+        try {
+            log.info('JWT strategy', jwt_payload);
+            return done(null, jwt_payload);
+        } catch (e) {
+            log.error('JWT strategy', e);
+            return done(null, false, {message: e});
+        }
     }
 ));
 
@@ -154,9 +112,9 @@ app.use(passport.initialize());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.post('/verify', passport.authenticate('jwt', {session: false}, (req, res) => {
-        res.send('User with name ' + req.user.name + ' is verified!');
-    })
+app.post('/verify', passport.authenticate('jwt', {session: false}), (req, res) => {
+        res.send('User with name ' + req.headers + ' is verified!');
+    }
 );
 
 app.post('/add', async (req, res) => {
