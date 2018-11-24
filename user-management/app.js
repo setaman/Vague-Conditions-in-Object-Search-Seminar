389bee295ca29189ae567e1f7540e2622d49bac1
Sergej Atamantschuk <sergej.atamantschuk@isst.fraunhofer.de>
2018-11-24 09:11:59 -0800

    user management

--- user-management/app.js
+++ user-management/app.js
@@ -1,16 +1,70 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let cors = require('cors');
+let dbConnector = require('./user/user');
+const MongoClient = require('mongodb').MongoClient;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var jwt = require('jsonwebtoken');
+let passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
 
-var app = express();
+var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwtSecret = 'secret';
+
+let db;
+
+// Connection URL
+const url = 'mongodb://localhost:27017/user';
+
+// Database Name
+const dbName = 'usersDb';
+
+// Create a new MongoClient
+
+const client = new MongoClient('mongodb://mongo:27017', { useNewUrlParser: true });
+
+client.connect(function (err) {
+
+    if (err) {
+        console.error('Can not connect to db:', err);
+    } else {
+        console.log("Connected successfully to server");
+        db = client.db(dbName);
+        client.close();
+    }
+});
+
+
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+
+        /*db.findOne({username}, (err, user) => {
+            if (err) {
+                return done(err);
+            }
+
+            if (!user.password !== passport) {
+                return done(null, false, {message: 'Incorrect password.'});
+            }
+
+            return done(null, user);
+        })*/
+    }
+));
+
+let app = express();
+
+app.use(cors());
+
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
