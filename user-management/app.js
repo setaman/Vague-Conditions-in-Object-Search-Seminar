70d8c67f532f358e093ada5b29e127850373bc6c
Sergej Atamantschuk <sergej.atamantschuk@isst.fraunhofer.de>
2018-11-24 12:25:16 -0800

    added passport auth, jwt, nodemon

--- user-management/app.js
+++ user-management/app.js
@@ -1,49 +1,72 @@
 let express = require('express');
 let path = require('path');
-let cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 let logger = require('morgan');
 let cors = require('cors');
-let dbConnector = require('./user/user');
 const MongoClient = require('mongodb').MongoClient;
 
-var jwt = require('jsonwebtoken');
 let passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy;
 
-var JwtStrategy = require('passport-jwt').Strategy,
+let JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
-const jwtSecret = 'secret';
 
 let db;
-
-// Connection URL
-const url = 'mongodb://localhost:27017/user';
 
 // Database Name
 const dbName = 'usersDb';
 
 // Create a new MongoClient
 
-const client = new MongoClient('mongodb://mongo:27017', { useNewUrlParser: true });
+const client = new MongoClient('mongodb://localhost:27017/users', {useNewUrlParser: true});
 
 client.connect(function (err) {
 
     if (err) {
         console.error('Can not connect to db:', err);
     } else {
-        console.log("Connected successfully to server");
+        console.log("Connected successful to server");
         db = client.db(dbName);
         client.close();
     }
 });
 
+passport.use('signup', new LocalStrategy(
+    async (username, password, done) => {
+        console.log(username);
+        let user;
+        try {
+            user = await db.collection('users').findOne({username});
+            console.log({user});
+            if (user) {
+                console.log(user);
+                return done(null, false, {message: 'User already exist'});
+            }
+        } catch (e) {
+            return done(null, false, {message: e});
+        }
 
-passport.use(new LocalStrategy(
-    (username, password, done) => {
+        try {
+            user = await db.collection('users').insertOne({username, password});
+            if (!user) {
+                return done(null, false, {message: 'Some Error occurred while inserting new user.'});
+            }
+            return done(null, {message: 'User' + user.name + ' successfully added'});
+        } catch (e) {
+            return done(null, false, {message: e});
+        }
+    }
+));
 
-        /*db.findOne({username}, (err, user) => {
+passport.use(new JwtStrategy({
+        secretOrKey: 'secret',
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+    },
+    (jwt_payload, done) => {
+
+        db.findOne({name: jwt_payload.name}, (err, user) => {
             if (err) {
-                return done(err);
+                return done(null, false, {message: 'Incorrect username.'});
             }
 
             if (!user.password !== passport) {
@@ -51,7 +74,7 @@ passport.use(new LocalStrategy(
             }
 
             return done(null, user);
-        })*/
+        })
     }
 ));
 
@@ -62,13 +85,25 @@ app.use(cors());
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.post('/verify', passport.authenticate('jwt', {session: false}, (req, res) => {
+        res.send('User with name ' + req.user.name + ' is verified!');
+    })
+);
+
+
+//Handle errors
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({error: err});
+});
 
 module.exports = app;
--- user-management/routes/users.js
+++ user-management/routes/users.js
@@ -1,9 +1,44 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.post('/login', async (req, res, next) => {
+    console.log('LOGIN');
+    passport.authenticate('local',
+        async (err, user) => {
+            if (err) {
+                const error = new Error('An Error occurred');
+                return next(error);
+            }
+            if (!user) {
+                const error = new Error('User ' + req.body.name + ' not found');
+                return next(error);
+            }
+
+            req.login(user, {session: false}, async (error) => {
+                if (error) return next(error);
+
+                const token = jwt.sign({name: user.name, _id: user._id}, 'secret');
+
+                return res.json({token});
+            })
+
+        })(req, res, next);
+});
+
+router.post('/signup', passport.authenticate('signup', { session : false}), async (req, res) => {
+    res.json({
+        massage: 'Signup successful',
+        user: req.body,
+    });
 });
 
 module.exports = router;
+
