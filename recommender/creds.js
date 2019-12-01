67be8515bb5c8eb62f456f645897a5bd5fadfddb
Sergej Atamantschuk <sergejatam@gmail.com>
2019-12-01 12:43:48 +0100

    add creds via .env file to docker-compose.yml

---
+++ recommender/creds.js
@@ -0,0 +1,8 @@
+console.log(process.env.RECOMBEE_DB);
+module.exports = {
+    neo4j_user: 'neo4j',
+    neo4j_pass: 'admin',
+    recommbe_db: process.env.RECOMBEE_DB,
+    recommbe_token: process.env.RECOMBEE_TOKEN,
+    tmdb_key: process.env.TMDB_API_KEY,
+};
